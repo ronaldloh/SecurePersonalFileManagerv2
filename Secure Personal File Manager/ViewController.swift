8f14e3b93cc6299f2a3a9fb835fda0fb2a3ff24a
Jan How <JanHow@Yong-Shin-Mins-MacBook-Air.local>
2015-04-19 15:12:30 +0800

    Initial Commit

---
+++ Secure Personal File Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Secure Personal File Manager
+//
+//  Created by Jan How on 4/19/15.
+//  Copyright (c) 2015 Loh Jan How. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Secure Personal File ManagerTests/Secure_Personal_File_ManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Secure_Personal_File_ManagerTests.swift
+//  Secure Personal File ManagerTests
+//
+//  Created by Jan How on 4/19/15.
+//  Copyright (c) 2015 Loh Jan How. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Secure_Personal_File_ManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
